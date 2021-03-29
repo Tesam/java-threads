fc542fb40c30220c82c802498cfe60af800f3853
Tesam <mtsamudiog@gmail.com>
2021-03-29 08:09:19 -0400

    refactor(Productor.java): add a critical region with a synchronized bloc

--- java-threads/src/ThreadsExamples/Productor.java
+++ java-threads/src/ThreadsExamples/Productor.java
@@ -18,20 +18,21 @@ public class Productor implements Runnable{
         this.sharedMemory = sharedMemory;
     }
     
-    private synchronized void write(){
-            
-        sharedVariable = (int) (Math.random()*(10-1)+1);
-            
-        this.sharedMemory.setSharedMemory(sharedVariable);
-            
-        System.out.println("El productor "+Thread.currentThread().getName()+" ingresó el número: "+sharedVariable);
+    private void write(){
         
-        try {
-            Thread.sleep(2000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        sharedVariable = (int) (Math.random()*(10-1)+1);
+        
+        synchronized(sharedMemory){
+            sharedMemory.setSharedMemory(sharedVariable);
+            System.out.println("El productor "+Thread.currentThread().getName()+" ingresó el número: "+sharedVariable);
+        
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
-         
+        
     } 
     
     @Override
