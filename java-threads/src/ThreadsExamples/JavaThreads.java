b19e872340aa80bd6e41f19e6f43b105edd81166
Tesam <mtsamudiog@gmail.com>
2021-03-28 21:41:03 -0400

    refactor(java.threads): remane the package to ThreadsExamples

---
+++ java-threads/src/ThreadsExamples/JavaThreads.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ThreadsExamples;
+
+/**
+ *
+ * @author Techi
+ */
+public class JavaThreads {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        SharedMemory sharedMemory = new SharedMemory();
+        
+        Productor productor = new Productor(sharedMemory);        
+        Consumidor consumidor = new Consumidor(sharedMemory);
+        
+        Thread threadProductor = new Thread(productor);
+        Thread threadConsumidor = new Thread(consumidor);
+        
+        threadProductor.start();
+        threadConsumidor.start();
+
+    }
+    
+}
