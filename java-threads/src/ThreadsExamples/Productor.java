24aea9bd1fe092d5682425e502a0ab0d7fcea425
Tesam <mtsamudiog@gmail.com>
2021-03-29 08:18:38 -0400

    doc(Productor.java): add documentation

--- java-threads/src/ThreadsExamples/Productor.java
+++ java-threads/src/ThreadsExamples/Productor.java
@@ -7,7 +7,7 @@ package ThreadsExamples;
 
 /**
  *
- * @author Bryan
+ * @author Techi
  */
 public class Productor implements Runnable{
 
@@ -18,17 +18,21 @@ public class Productor implements Runnable{
         this.sharedMemory = sharedMemory;
     }
     
+    /*
+    * Método que escribe en la memoria compartida
+    */
     private void write(){
         
         sharedVariable = (int) (Math.random()*(10-1)+1);
         
+        //Región crítica del hilo        
         synchronized(sharedMemory){
             sharedMemory.setSharedMemory(sharedVariable);
             System.out.println("El "+Thread.currentThread().getName()+" ingresó el número: "+sharedVariable);
         }
         
         try {
-                Thread.sleep(2000);
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             System.out.println("INTERRUPTED EXCEPTION "+e.getMessage());
         }
@@ -36,6 +40,7 @@ public class Productor implements Runnable{
     } 
     
     @Override
+    //El hilo se ejecuta 10 veces
     public void run() {
         for(int i = 0; i < 10; i++){
             write();
