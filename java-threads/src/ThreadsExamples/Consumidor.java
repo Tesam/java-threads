59fa182c9b50cd8b91e03bbcbdd0709a5112aae4
Tesam <mtsamudiog@gmail.com>
2021-03-29 14:02:28 -0400

    refactor(Consumidor.java): add critical region with synchronized block

--- java-threads/src/ThreadsExamples/Consumidor.java
+++ java-threads/src/ThreadsExamples/Consumidor.java
@@ -11,28 +11,34 @@ package ThreadsExamples;
  */
 public class Consumidor implements Runnable{
     
-    SharedMemory sharedMemory;
+    final SharedMemory sharedMemory;
     int sharedVariable;
     
     Consumidor(SharedMemory sharedMemory){
         this.sharedMemory = sharedMemory;
     }
     
-    private synchronized void read(){         
+    /*
+    * Método que lee en la memoria compartida
+    */
+    private void read(){         
       
-        sharedVariable = this.sharedMemory.getSharedMemory();
-            
-        System.out.println("El consumidor "+Thread.currentThread().getName()+" lee el número: "+sharedVariable);
+        //Región crítica del hilo
+        synchronized(sharedMemory){
+            sharedVariable = this.sharedMemory.getSharedMemory();
+            System.out.println("El "+Thread.currentThread().getName()+" lee el número: "+sharedVariable);
+        }
         
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            System.out.println("INTERRUPTED EXCEPTION "+e.getMessage());
         }
          
     }
 
     @Override
+    //El hilo se ejecuta 10 veces
     public void run() {
         for(int i = 0; i < 10; i++){
             read();
