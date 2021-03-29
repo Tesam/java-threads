6c328c4b602b339e26d02ab54c703c3c020b8af4
Tesam <mtsamudiog@gmail.com>
2021-03-28 21:40:49 -0400

    refactor(java.threads): remane the package to ThreadsExamples

---
+++ java-threads/src/ThreadsExamples/Productor.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ThreadsExamples;
+
+/**
+ *
+ * @author Bryan
+ */
+public class Productor implements Runnable{
+
+    SharedMemory sharedMemory;
+    int sharedVariable;
+    
+    Productor(SharedMemory sharedMemory){
+        this.sharedMemory = sharedMemory;
+    }
+    
+    private synchronized void write(){
+            
+        sharedVariable = (int) (Math.random()*(10-1)+1);
+            
+        this.sharedMemory.setSharedMemory(sharedVariable);
+            
+        System.out.println("El productor ingresó el número: "+sharedVariable);
+         
+    } 
+    
+    @Override
+    public void run() {
+        write();
+    }
+    
+}
